c7ee5060db083bd1c9be267ab8cf33dd0c788872
Aman860-spec <abdiel1mendezaux@gmail.com>
2025-03-19 10:08:39 -0600

    things news 2

--- src/controllers/formController.js
+++
@@ -1,7 +0,0 @@
-// const connection = require('../config/db');
-
-exports.enviarFormulario = (req, res) => {
-  const { email, password} = req.body;
-
-  // Logica para enviar email y password
-};
--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const formController = require('../controllers/formController');
+// const formController = require('../controllers/formController');
+import {enviarFormulario} from '../controllers/formController.js'
 
 const router = express.Router();
 
 // Ruta para manejar el envío del formulario
-router.post('/enviar-formulario', formController.enviarFormulario);
+router.post('/enviar-formulario', enviarFormulario);
 
 module.exports = router;
